9fe6524935e204693bac9e4f7913183b39179904
Braydon <bray.200913@gmail.com>
2026-06-21 00:16:01 -0400

    fix: manual mode approve flow, env var fallbacks, MAKE_WEBHOOK guard, hardcoded URLs

--- Procfile
+++
@@ -1,2 +0,0 @@
-web: node backend/server.js
-worker: node backend/bot.js
--- tools/copy_to_startup.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-copy "C:\Users\brayd\autopilot\startup_brief.vbs" "C:\Users\brayd\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\startup_brief.vbs"
-echo Done.
